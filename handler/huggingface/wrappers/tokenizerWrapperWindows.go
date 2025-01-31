678a87a0cd380c13628e1b3154f583768372d676
JonCSykes <joncsykes@gmail.com>
2025-01-31 12:18:38 -0500

    add compiled rust libtokenizer

--- handler/huggingface/wrappers/tokenizerWrapperWindows.go
+++ handler/huggingface/wrappers/tokenizerWrapperWindows.go
@@ -3,6 +3,6 @@
 package wrappers
 
 /*
-#cgo windows LDFLAGS: -L"${SRCDIR}/../lib/win_x64" -ltokenizers -lm -lstdc++ -lbcrypt -lws2_32 -lntdll
+#cgo windows LDFLAGS: -L"${SRCDIR}/../lib/win_x64" -ltokenizers -lm -lstdc++ -lbcrypt -lws2_32 -lntdll -luserenv
 */
 import "C"
